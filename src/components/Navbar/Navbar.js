4f5a2a963d255eaef65f208eda038588dec4b23d
Ian Bortnic <ianbortnic.uchile@gmail.com>
2022-07-19 13:51:27 -0400

    added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import './Navbar.css'
+import img_logo from './img/logo.png';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark background">
+        <div className="container-fluid">
+        <a href=" "><img src={img_logo} height="80px" alt="logo"/></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav navbar-custom">
+                <li className="nav-item"><a className="nav-link not_selected" href=" ">Supermercados</a></li>
+                <li className="nav-item"><a className="nav-link not_selected" href=" ">Locales</a></li>
+                <li className="nav-item"><a className="nav-link not_selected" href=" ">Ferias</a></li>
+                <li className="nav-item"><a className="nav-link not_selected" href=" ">Suplementos</a></li>
+            </ul>
+        </div>
+        </div>
+        </nav>
+    )
+}
+
+
+
+export default Navbar
