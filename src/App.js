fe909f07d05f8fb46075875e45a2343bc00e52a3
Ian Bortnic <ianbortnic.uchile@gmail.com>
2022-07-25 18:39:29 -0400

    added ItemCount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-// import Counter from './components/Counter/Counter';
+import Counter from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 
 function App() {
+
+  const handleOnAdd = (count) => {
+    console.log(`${count} Items agregados satisfactoriamente`)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        {/* <Counter/> */}
+        <Counter stock={20} initial={1} onAdd={handleOnAdd}/>
         <ItemListContainer greeting="¡Bienvenido a Vegin Market, el mejor mercado vegano!"/>
 
       </header>
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './ItemCount.css';
+
+
+
+const Counter = ({stock, initial, onAdd}) => {
+
+    const [count, setCount] = useState(0)
+
+
+    const decrement = () => {
+        if(count > initial){
+            setCount((prevCount) => prevCount - 1)
+        }
+
+
+
+    }
+
+
+    const increment = () => {
+
+        if(count < stock){
+            setCount((prevCount) => prevCount + 1)
+        }
+
+    }
+
+
+
+    return (
+    <div className='container_father'>
+        <h1>{count}</h1>
+        <div className='count_container'>
+
+            <buttton className='counter_button' onClick={decrement}>-</buttton>
+            <buttton className='counter_button' onClick={increment}>+</buttton>
+
+        </div>
+        <div className='count_container'>
+            <buttton className='counter_button' onClick={()=> onAdd(count)}>Agregar al Carrito</buttton>
+        </div>
+
+
+
+    </div>
+    )
+}
+
+export default Counter
+
