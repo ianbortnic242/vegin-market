f51b72c73b3f61f4349261dd362eb58eb672611d
Ian Bortnic <ianbortnic.uchile@gmail.com>
2022-09-15 16:38:15 -0300

    able to receive categories from firebase

--- src/components/main/Home/Home.js
+++ src/components/main/Home/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
 
 
     const [home_img, setHomeImg] = useState([])
+    const [sections, setSections] = useState([])
+
 
     useEffect(()=>{
 
@@ -21,13 +23,58 @@ const Home = () => {
         })}
     ,[])
 
+    useEffect(() => {
+        const collectionRef = collection(db, "categories_vegin");
+        getDocs(collectionRef).then((response) => {
+            const aprender_categories = []
+            const actuar_categories = []
+            response.docs.forEach((category) => {
+                const data = category.data()
+                if(data.section==='Aprender'){
+                    aprender_categories.push(data);
+                }
+                if(data.section==='Actuar'){
+                    actuar_categories.push(data);
+                }
+            })
+            return [aprender_categories, actuar_categories];
+        }).then((categories) =>{
+            setSections([{'name':"Aprender", 'categories': categories[0], 'color': '#72D3AD'},{'name':"Actuar", 'categories': categories[1], 'color': '#2D9E8A'}])
+        }).catch(err =>{
+                console.log(err)
+            })
+    },[])
+
 
 
     return (
 
         <div className='container_box'>
             <div><img className="box" src = {home_img}/></div>
-            <div className='box stack-top main_div'></div>
+            <div className='box stack-top main_div'>
+                <p>Escoge un tema para comenzar!</p>
+
+                {sections.map((section) => (
+                    <section className="section">
+                        <h2 style={{'color': section['color']}}>{section['name']}</h2>
+                        <div>
+                            <div className="container-fluid overflow-scroll">
+                                <div className="row d-flex justify-content-around flex-nowrap">
+                                    {section['categories'].map((category) => (
+                                        <div className="col-4 d-flex justify-content-around">
+                                            <div className="card card-block index_topics_size topics">
+                                                <div><img className="fill_div transition" src={category.img}/></div>
+                                                <h1 className="centered_down">{category.name}</h1>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                ))}
+
+            </div>
         </div>
 
 )}
