80d74a1a3e519c64e77fc2960e6102c66a60db2a
Ian Bortnic <ianbortnic.uchile@gmail.com>
2023-01-15 18:22:44 -0300

    article structure example

--- src/components/main/Categories/Introduction/SubCategory/Article/Article.js
+++ src/components/main/Categories/Introduction/SubCategory/Article/Article.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {useParams} from "react-router-dom";
 import './Article.css'
 
@@ -5,10 +6,41 @@ const Article = () => {
     // We can use the `useParams` hook here to access
     // the dynamic pieces of the URL.
     let { id } = useParams();
+
+    const home_img = "https://www.w3schools.com/images/picture.jpg"
+
+    const texto = "woubcvwoeuvbwrouubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nbwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nbwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nbwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nwoubcvwoeuvbwrouweofnweoucbwouvcbw /nb"
+
+    const ReadMoreReadLess = (children) => {
+
+      const [isReadMoreShown, SetReadMoreSHown] = useState(false)
+
+      const toggleBtn = () => {
+        SetReadMoreSHown(prevState => !prevState)
+
+      }
+
+      return (
+        <p>
+          {isReadMoreShown ? children : children.substring(0, 200)}<br/>
+          {children.length>200 ? <button className="btn_read_more_read_less" onClick={toggleBtn}>{isReadMoreShown? 'Leer Menos': 'Leer Mas...'}</button> : null}
+        </p>
+      )
+    }
+
   
     return (
-      <div>
-        <h3>ID: {id}</h3>
+      
+
+      <div className='container_box'>
+        <div><img alt='' className="box" src = {home_img}/></div>
+        <div className='box_2 stack-top main_div bk_color wow'>
+          <h2>¿Por que veganismo?</h2> <br/>
+          <p>{ReadMoreReadLess(texto)}</p>
+
+
+        </div>
+        
       </div>
     );
   }
